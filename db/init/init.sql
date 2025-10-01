bb5bdef1a3628114ccdb85efa210cf986d2d5577
Ville Majava <vikke.majava@gmail.com>
2025-10-01 17:50:07 +0300

    mysql_native_password

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS appdb;
-CREATE USER IF NOT EXISTS 'appuser'@'%' IDENTIFIED WITH caching_sha2_password BY 'changeme';
+CREATE USER IF NOT EXISTS 'appuser'@'%' IDENTIFIED WITH mysql_native_password BY 'changeme';
 GRANT ALL PRIVILEGES ON appdb.* TO 'appuser'@'%';
 FLUSH PRIVILEGES;
