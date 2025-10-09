ddaa804ba9f7e51ded7017b4e8adeabb2a3b39d4
Ville Majava <vikke.majava@gmail.com>
2025-10-09 12:58:52 +0300

    response.status_code

--- backend/test_app.py
+++ backend/test_app.py
@@ -14,7 +14,7 @@ class BasicTests(unittest.TestCase):
     def test_clock(self):
         response = self.app.get('/api/time')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'time', response.data)
+        self.assertIn(b'time', response.status_code)
 
 
 
