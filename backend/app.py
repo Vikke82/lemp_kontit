921cd0339f0fea1986344b42212b878182424752
Ville Majava <vikke.majava@gmail.com>
2025-12-11 18:47:21 +0200

    env poistettu

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 import os
 import mysql.connector
 
-#flask app instance update
+#flask app instance update joo
 app = Flask(__name__)
 
 DB_HOST = os.getenv('DB_HOST', 'db')
