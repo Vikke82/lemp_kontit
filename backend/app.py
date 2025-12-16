8e23d6224aa7529ee5c81db8437715c4b8f95210
Ville Majava <vikke.majava@gmail.com>
2025-12-16 18:28:23 +0200

    josnify api/time

--- backend/app.py
+++ backend/app.py
@@ -28,7 +28,7 @@ def time():
     cur.execute("SELECT NOW()")
     row = cur.fetchone()
     cur.close(); conn.close()
-    return jsonify(row)
+    return jsonify(message=row[0])
 
 @app.get('/api')
 def index():
